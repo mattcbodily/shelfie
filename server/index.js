e013a3f1f4b53877bc099461080011daa7f5defc
Matthew Bodily <matthewbodily@Matthews-MacBook-Air.local>
2019-01-25 10:20:29 -0700

    database setup finished

--- server/index.js
+++ server/index.js
@@ -1,8 +1,14 @@
 const express = require('express');
+require('dotenv').config();
 const bodyParser = require('body-parser');
+const massive = require('massive');
 
 const app = express();
 app.use(bodyParser.json())
+
+massive(process.env.CONNECTION_STRING)
+.then(db => app.set('db', db))
+.catch(err => console.log('Error!', err))
 
 
 app.listen(4000, () => {
